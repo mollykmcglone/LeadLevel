66bd35e233582d8c65a1e7e1d542ba2450db2b5a
Molly <mollykmcglone@gmail.com>
2016-07-27 10:14:20 -0700

    fix route to display search result by name

--- app.rb
+++ app.rb
@@ -8,24 +8,33 @@ get('/') do
   erb(:index)
 end
 
-post('/places') do
-  address = params['address']
+post('/places/name') do
   name = params['name']
-  @@new_places = Place.where(address_line1: address)
-  if @@new_places != []
-    redirect('/places')
+  @place = Place.find_by(name: name)
+  if @place != ''
+    redirect('/places/'.concat(@place.id().to_s()))
   else
-    @@new_places = Place.where(name: name)
-    if @@new_places != []
-      redirect('/places')
-    else
-      erb(:places_form)
-    end
+    erb(:places_form)
   end
 end
 
-get('/places') do
-  @@new_places
+post('/places/address') do
+  street_number = params['street_number']
+  street_name = params['street_name']
+  street_direction = params['street_direction']
+  street_type = params['street_type']
+  apt = params['apt']
+  address = street_number.concat(street_name).concat(street_direction).concat(street_type).concat(apt)
+  @new_place = Place.where(address_line1: address)
+  if @new_place
+    redirect('/places/'.concat(@new_place.id().to_s()))
+  else
+    erb(:places_form)
+  end
+end
+
+get('/places/:id') do
+  @place = Place.find(params['id'].to_i())
   erb(:places)
 end
 
