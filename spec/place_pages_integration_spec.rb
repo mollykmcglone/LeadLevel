a8954583e40ac0c94a5e4f431f7565b9467194d1
Alex Jacks and Bang Nguyen <alex+bang@>
2016-07-28 11:36:52 -0700

    adds contact form fields to new place form & places table

--- spec/place_pages_integration_spec.rb
+++ spec/place_pages_integration_spec.rb
@@ -10,13 +10,13 @@ describe('the places route', :type => :feature) do
   end
 
   it "allows the user to search for a place by address" do
-    test_place = Place.create({:name => "CIBO", :address_line1 => "3444 SE DIVISION STREET", :city => 'PORTLAND', :state => 'OREGON'})
+    test_place = Place.create({:name => "", :address_line1 => "3936 SE SHERMAN STREET", :city => 'PORTLAND', :state => 'OREGON'})
     visit '/'
-    fill_in 'street_number', :with => '3444'
-    select 'SE', :from => 'street_direction'
-    fill_in 'street_name', :with => 'Division'
-    select 'Street', :from => 'street_type'
+    fill_in 'street_number', :with => '3936'
+    select('SE', :from => 'street_direction')
+    fill_in 'street_name', :with => 'SHERMAN'
+    select('Street', :from => 'street_type')
     click_button 'Find this Address'
-    expect(page).to have_content('Cibo')
+    expect(page).to have_content("SHERMAN")
   end
 end
